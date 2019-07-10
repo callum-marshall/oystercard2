12ebdd0240df5e15765346d9b6584003b9f93a92
callum-marshall <callum_marshall@hotmail.co.uk>
2019-07-10 17:50:53 +0100

    refactor oystercard_spec

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,16 +2,16 @@ require 'oystercard'
 
 describe Oystercard do
 
+  let(:card) { Oystercard.new }
+
   describe '#balance' do
     it 'starts with a default balance of 0' do
-      card = Oystercard.new
       expect(card.balance).to eq 0
     end
   end
 
   describe 'top_up' do
     it 'adds the given amount to the balance' do
-      card = Oystercard.new
       card.top_up(10)
       expect(card.balance).to eq 10
     end
