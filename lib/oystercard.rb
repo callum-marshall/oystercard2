63365816b7b994127021ee747182d81d29e024cf
callum-marshall <callum_marshall@hotmail.co.uk>
2019-07-11 13:02:09 +0100

    add deduct and refactor

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,20 @@
 class Oystercard
 
+  MAX_BAL = 90
+
+  attr_reader :balance
+
   def initialize
     @balance = 0
   end
-
-  attr_reader :balance
 
   def top_up(amount)
     raise("You cannot exceed £#{MAX_BAL}") if (@balance + amount) > MAX_BAL
     @balance += amount
   end
 
-  MAX_BAL = 90
+  def deduct(amount)
+    @balance -= amount
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,9 +15,17 @@ describe Oystercard do
     it 'adds the given amount to the balance' do
       expect(card.top_up(10)).to eq 10
     end
+
     it 'throws an exception if the new balance exceeds the maximum balance' do
-      card.top_up(50)
-      expect { card.top_up(50) }.to raise_error("You cannot exceed £#{max_bal}")
+      card.top_up(max_bal)
+      expect { card.top_up(1) }.to raise_error("You cannot exceed £#{max_bal}")
+    end
+  end
+
+  describe '#deduct' do
+    it 'deducts the given amount from the balance' do
+      card.top_up(20)
+      expect(card.deduct(5)).to eq 15
     end
   end
 end
