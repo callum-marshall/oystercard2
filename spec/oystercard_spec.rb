c454cd027bf11557b8e396b85843ff6e8a7d9747
callum-marshall <callum_marshall@hotmail.co.uk>
2019-08-01 19:24:33 +0100

    refactor tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,14 +3,20 @@ require 'oystercard'
 describe Oystercard do
 
   let(:card) { Oystercard.new }
-  let(:max_bal) { Oystercard::MAX_BAL }
-  let(:min_fare) { Oystercard::MIN_FARE }
   let(:entry_station) { double :station }
   let(:exit_station) { double :station }
   let(:journey) { { entry_station: entry_station, exit_station: exit_station } }
 
   it 'has an empty list of journeys by default' do
     expect(subject.journey_history).to be_empty
+  end
+
+  it 'starts with a default balance of 0' do
+    expect(card.balance).to eq 0
+  end
+
+  it 'starts not "in journey"' do
+    expect(card).not_to be_in_journey
   end
 
   it 'stores a journey' do
@@ -20,51 +26,4 @@ describe Oystercard do
     expect(card.journey_history).to include journey
   end
 
-  it 'starts with a default balance of 0' do
-    expect(card.balance).to eq 0
-  end
-
-  describe '#top_up' do
-    it 'adds the given amount to the balance' do
-      expect(card.top_up(10)).to eq 10
-    end
-
-    it 'throws an exception if the new balance exceeds the maximum balance' do
-      card.top_up(max_bal)
-      expect { card.top_up(1) }.to raise_error("You cannot exceed £#{max_bal}")
-    end
-  end
-
-  describe '#in_journey?' do
-    it 'starts not in journey' do
-      expect(card).not_to be_in_journey
-    end
-  end
-
-  describe '#touch_in' do
-    it 'changes in_journey to true' do
-      card.top_up(10)
-      card.touch_in(entry_station)
-      expect(card).to be_in_journey
-    end
-
-    it 'throws an exception if a card has insufficient balance' do
-      expect { card.touch_in(entry_station) }.to raise_error("You have insufficient funds")
-    end
-  end
-
-  describe '#touch_out' do
-    it 'changes in_journey to false' do
-      card.top_up(10)
-      card.touch_in(entry_station)
-      card.touch_out(exit_station)
-      expect(card).not_to be_in_journey
-    end
-
-    it 'deducts the minimum fare' do
-      card.top_up(10)
-      card.touch_in(entry_station)
-      expect { card.touch_out(exit_station) }.to change { card.balance }.by(-5)
-    end
-  end
 end
