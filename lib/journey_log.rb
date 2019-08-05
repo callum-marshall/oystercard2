7dac0189ff54672f2c338ae872474047e0983a4d
callum-marshall <callum_marshall@hotmail.co.uk>
2019-08-05 19:55:54 +0100

    add journey_log.rb

---
+++ lib/journey_log.rb
@@ -0,0 +1,15 @@
+class JourneyLog
+
+  attr_reader :journeys
+
+  def initialize(journey_class:)
+    @journey_class = journey_class
+    @journeys = []
+  end
+
+  def start(station)
+    @journeys << @journey_class.new(entry_station: station)
+
+  end
+
+end
